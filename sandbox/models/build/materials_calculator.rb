08cf563724cb297da047c643e94b30bc2ba471f0
Jesse Hill <jessebhill@gmail.com>
2013-11-25 21:42:57 -0500

    Updated materials calculator

--- sandbox/models/build/materials_calculator.rb
+++ sandbox/models/build/materials_calculator.rb
@@ -7,49 +7,33 @@ class MaterialsCalculator
 	end
 
 	def required_materials(blueprint, runs)
-		materials = Hash.new(0)
 		inv_type = blueprint.inv_type
-		ram_requirements = inv_type.ram_type_requirements_for_manufacturing
+		ram_requirements = inv_type.ram_type_requirements_for_manufacturing.select { |r| !r.required_type.is_skill? }
 
-		# Start by collecting all the 'base' materials
-		inv_type.inv_type_materials.each { |m| materials[m.required_type.typeID] = m.quantity }
-
-		# Remove recyleable materials, don't ask me why the BPO requirements are set up this way, but they are.
-		remove_recyleable_ram_requirements(materials, ram_requirements)
-
-		# Now adjust for waste and quantity
-		materials.each { |typeID, quantity|
-			materials[typeID] = @waste_calculator.calculate_required_quantity(inv_type, quantity) * runs
-		}
+		materials = Hash[inv_type.inv_type_materials. # Create a hash of [type: quantity] from the following ...
+			each_with_object({}) { |m, h| h[m.required_type] = m.quantity }. # collect base materials
+			merge(recyleable_requirements(ram_requirements)) { |k, l, r| l - r }. # remove recycleable materials
+			select { |k, v| v > 0}. # remove empty elements
+			map { |type, quantity| # Adjust for waste and number of runs
+				[type, @waste_calculator.calculate_required_quantity(inv_type, quantity) * runs]
+			}]
 
 		# Now add in ram requirements (extra materials) which are not skills
 		# Apply production efficienty waste to materials which are already in the base material list.
-		ram_requirements.each { |ram_requirement|
-			next if ram_requirement.required_type.is_skill?
-			requiredQuantity = ram_requirement.quantity * runs
-			if materials.has_key?(ram_requirement.required_type.typeID)
-				materials[ram_requirement.required_type.typeID] = materials[ram_requirement.required_type.typeID] + 
-					requiredQuantity +
-					@waste_calculator.production_waste(requiredQuantity)
-			else
-				materials[ram_requirement.required_type.typeID] = requiredQuantity
-			end
-		}
-
-		materials
+		ram_requirements.
+			each_with_object({}) { |r, h| h[r.required_type] = r.quantity * runs }.
+			merge(materials) { |k, l, r| l + @waste_calculator.production_waste(l) + r }
 	end
 
-	def remove_recyleable_ram_requirements(materials, ram_requirements)
-		ram_requirements.each {|ram_requirement|
-			next if ram_requirement.required_type.is_skill?
-			remove_materials(materials, ram_requirement) if ram_requirement.recycle?
-		}
-	end
-
-	def remove_materials(materials, ram_requirement)
-		ram_requirement.required_type.inv_type_materials.each {|m|
-			materials[m.required_type.typeID] -= m.quantity
-			materials.delete(m.required_type.typeID) if materials[m.required_type.typeID] <= 0
-		}
+	def recyleable_requirements(ram_requirements)
+		ram_requirements.
+			select { |requirement| requirement.recycle? }.
+			each_with_object({}) { |requirement, requirements| 
+				materials = requirement.required_type.inv_type_materials.
+					each_with_object({}) { |m, materials|
+						materials[m.required_type] = m.quantity * requirement.quantity
+					}
+				requirements.merge!(materials) { |k, l, r| l + r }
+			}
 	end
 end	
--- sandbox/spec/models/build/materials_calculator_spec.rb
+++ sandbox/spec/models/build/materials_calculator_spec.rb
@@ -3,6 +3,7 @@ require_relative '../../../models/database/inv_type'
 require_relative '../../../models/build/materials_calculator'
 
 describe MaterialsCalculator do
+
 	before :each do
 		@blueprint_repo = mock('repo')
 		@waste_calculator = WasteCalculator.new(5, @blueprint_repo)
@@ -13,14 +14,40 @@ describe MaterialsCalculator do
 		blueprint = InvType.find_by_typeName('Worm').inv_blueprint_type
 		@blueprint_repo.expects(:material_level).returns(0).at_least_once
 		materials = @materials_calculator.required_materials(blueprint, 10)
-		puts materials
-		materials[34].should eq(208600)
-		materials[35].should eq(92880)
-		materials[36].should eq(30950)
-		materials[37].should eq(5840)
-		materials[38].should eq(20)
-		materials[39].should eq(20)
-		materials[40].should eq(20)
+
+		materials.length.should eq(7)
+		materials[InvType.find_by_typeName('Tritanium')].should eq(208600)
+		materials[InvType.find_by_typeName('Pyerite')].should eq(92880)
+		materials[InvType.find_by_typeName('Mexallon')].should eq(30950)
+		materials[InvType.find_by_typeName('Isogen')].should eq(5840)
+		materials[InvType.find_by_typeName('Nocxium')].should eq(20)
+		materials[InvType.find_by_typeName('Zydrine')].should eq(20)
+		materials[InvType.find_by_typeName('Megacyte')].should eq(20)
 	end	
+
+	it 'should calculate materials for a tech II item correctly' do
+		blueprint = InvType.find_by_typeName('Crow').inv_blueprint_type
+		@blueprint_repo.expects(:material_level).returns(-4).at_least_once
+		materials = @materials_calculator.required_materials(blueprint, 1)
+
+		materials.length.should eq(17)
+		materials[InvType.find_by_typeName('Tritanium')].should eq(300)
+		materials[InvType.find_by_typeName('Pyerite')].should eq(300)
+		materials[InvType.find_by_typeName('Mexallon')].should eq(30)
+		materials[InvType.find_by_typeName('Isogen')].should eq(30)
+		materials[InvType.find_by_typeName('Zydrine')].should eq(15)
+		materials[InvType.find_by_typeName('Megacyte')].should eq(15)
+		materials[InvType.find_by_typeName('Morphite')].should eq(8)
+		materials[InvType.find_by_typeName('Construction Blocks')].should eq(15)
+		materials[InvType.find_by_typeName('Magpulse Thruster')].should eq(45)
+		materials[InvType.find_by_typeName('Gravimetric Sensor Cluster')].should eq(30)
+		materials[InvType.find_by_typeName('Quantum Microprocessor')].should eq(120)
+		materials[InvType.find_by_typeName('Titanium Diborite Armor Plate')].should eq(450)
+		materials[InvType.find_by_typeName('Graviton Reactor Unit')].should eq(5)
+		materials[InvType.find_by_typeName('Scalar Capacitor Unit')].should eq(75)
+		materials[InvType.find_by_typeName('Sustained Shield Emitter')].should eq(23)
+		materials[InvType.find_by_typeName('Condor')].should eq(1)
+		materials[InvType.find_by_typeName('R.A.M.- Starship Tech')].should eq(1)
+	end		
 
 end
