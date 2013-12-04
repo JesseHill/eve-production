9541191dbcc92a70fae37e7d5dd53bec6fbf0416
Jesse Hill <jessebhill@gmail.com>
2013-12-03 20:16:07 -0500

    Updates market group spec

--- sandbox/spec/models/database/inv_market_group_spec.rb
+++ sandbox/spec/models/database/inv_market_group_spec.rb
@@ -4,8 +4,15 @@ require_relative '../../../models/database/inv_market_group.rb'
 describe InvMarketGroup do
 
 	it 'should answer the correct group for a group id' do
-		group = InvMarketGroup.find_by(marketGroupID: 5)
+		group = InvMarketGroup.find_by_marketGroupID(5)
 		group.marketGroupName.should eq('Standard Frigates')
 	end
 
+	it 'should answer whether a group is included in another' do
+		# 420 == Covert Ops
+		group = InvMarketGroup.find_by_marketGroupID(420)
+		group.included_in?(:advanced_frigates).should be_true
+		group.included_in?(:ships).should be_true
+		group.included_in?(:cruisers).should be_false
+	end
 end
